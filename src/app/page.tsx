ff8bff3ea87d648ed11856ded56cd8d48754d89d
Mrityunjay Yadav <mrityunjayy8001@gmail.com>
2025-07-08 23:32:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="grid g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import Hero from "@/components/landing/hero";
 import Services from "@/components/landing/services";
 import Testimonials from "@/components/landing/testimonials";
 import CTA from "@/components/landing/cta";
-import Pricing from "@/components/landing/pricing";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Pricing />
       <Services />
       <Testimonials />
       <CTA />
