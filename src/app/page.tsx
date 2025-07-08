07078a6d90ab18e465e112b9b97b53c977204cd9
Mrityunjay Yadav <mrityunjayy8001@gmail.com>
2025-07-08 19:33:07 +0000

    add secation Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Hero from "@/components/landing/hero";
 import Services from "@/components/landing/services";
+import Clients from "@/components/landing/clients";
+import Contact from "@/components/landing/contact";
 import CTA from "@/components/landing/cta";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <div className="dark">
       <Hero />
       <Services />
+      <Clients />
+      <Contact />
       <CTA />
     </div>
   );
