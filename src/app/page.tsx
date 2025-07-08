ce355ea825b11f4c8ce86440c3ebb2adf416201e
Mrityunjay Yadav <mrityunjayy8001@gmail.com>
2025-07-08 19:49:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Contact>
    
    with th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Hero from "@/components/landing/hero";
 import Services from "@/components/landing/services";
 import Clients from "@/components/landing/clients";
-import Contact from "@/components/landing/contact";
 import CTA from "@/components/landing/cta";
+import Testimonials from "@/components/landing/testimonials";
+import Portfolio from "@/components/landing/portfolio";
 
 export default function Home() {
   return (
     <div className="dark">
       <Hero />
       <Services />
+      <Portfolio />
+      <Testimonials />
       <Clients />
-      <Contact />
       <CTA />
     </div>
   );
