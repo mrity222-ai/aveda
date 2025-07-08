f0834ed2145e797134db5e144af1b22ca03c2626
Mrityunjay Yadav <mrityunjayy8001@gmail.com>
2025-07-08 22:41:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Hero from "@/components/landing/hero";
 import Services from "@/components/landing/services";
 import Testimonials from "@/components/landing/testimonials";
-import CTA from "@/components/landing/cta";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
-      <CTA />
       <Testimonials />
     </>
   );
