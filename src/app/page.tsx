8bba849108719f65a256b69b4555a0b5fef7ecbd
Mrityunjay Yadav <mrityunjayy8001@gmail.com>
2025-07-08 15:37:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Contact>
    
    with th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/landing/hero";
 import LogoCarousel from "@/components/landing/logo-carousel";
 import Services from "@/components/landing/services";
 import Testimonials from "@/components/landing/testimonials";
-import Contact from "@/components/landing/contact";
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <LogoCarousel />
       <Services />
       <Testimonials />
-      <Contact />
     </>
   );
 }
