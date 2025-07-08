5851a98a901351ca69538d0f95dfe28722137cd1
Mrityunjay Yadav <mrityunjayy8001@gmail.com>
2025-07-08 16:54:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Testimonials>
    
    wi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/landing/hero";
 import Services from "@/components/landing/services";
 import Clients from "@/components/landing/clients";
 import Portfolio from "@/components/landing/portfolio";
-import Testimonials from "@/components/landing/testimonials";
 import CTA from "@/components/landing/cta";
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
       <Hero />
       <Services />
       <Portfolio />
-      <Testimonials />
       <Clients />
       <CTA />
     </>
